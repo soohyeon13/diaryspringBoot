7f29743349b0bf00c1e7b9c69f667203a2604699
soohyeon13 <rlatngus6663@gmail.com>
2019-06-07 03:33:27 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/diaryspring/DiaryspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.diaryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryspringApplication.class, args);
+    }
+
+}
