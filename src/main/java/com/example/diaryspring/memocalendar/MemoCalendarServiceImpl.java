cb0f0196a7fb9bf9d3f97f57c0c1dbc4bea981e5
soohyeon13 <rlatngus6663@gmail.com>
2019-06-19 21:00:49 +0900

    MemoCalendar ServiceImpl

--- src/main/java/com/example/diaryspring/memocalendar/MemoCalendarServiceImpl.java
+++ src/main/java/com/example/diaryspring/memocalendar/MemoCalendarServiceImpl.java
@@ -1,4 +1,47 @@
 package com.example.diaryspring.memocalendar;
 
-public class MemoCalendarServiceImpl {
+
+import com.example.diaryspring.dto.MemoCalendarDto;
+import com.example.diaryspring.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemoCalendarServiceImpl implements MemoCalendarService {
+
+    private final MemoCalendarRepo memoCalendarRepo;
+    private final UserService userService;
+
+    @Override
+    public List<MemoCalendar> findAllByUserId(Integer memo_user_id) {
+        List<MemoCalendar> memoCalendars = memoCalendarRepo.findAllByUserId(memo_user_id);
+        return memoCalendars;
+    }
+
+    @Override
+    public MemoCalendar updateCalendar(MemoCalendar memoCalendar) {
+        return memoCalendarRepo.save(memoCalendar);
+    }
+
+    @Override
+    public void deleteCalendar(Integer id) {
+        memoCalendarRepo.deleteById(id);
+    }
+
+    @Override
+    public MemoCalendar addCalendar(MemoCalendarDto memoCalendarDto) {
+        MemoCalendar memoCalendar =memoCalendarDto.getMemoCalendar();
+        memoCalendar.setUser(userService.findByid(memoCalendarDto.getMemo_user_id()));
+        return memoCalendarRepo.save(memoCalendar);
+    }
+
+    @Override
+    public List<MemoCalendar> calendarList() {
+        return memoCalendarRepo.findAll();
+    }
 }
+
+
